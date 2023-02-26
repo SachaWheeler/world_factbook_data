c24ce25239d2d9a96ef48b84e5c9673f637278f9
Sacha Wheeler <sacha@sachawheeler.com>
2023-02-26 22:01:10 +0000

    working

--- correlate.py
+++ correlate.py
@@ -2,14 +2,20 @@ import glob
 import pprint
 import os
 import pandas as pd
+import itertools
 
 
 data_files = [file for file in glob.glob("data/*.csv")]
 # pprint.pprint(data_files)
+cols = [name.split("/")[1].split(".")[0] for name in data_files]
+#pprint.pprint(cols)
 
 combined_df = None
 for file in data_files:
-    df = pd.read_csv(file, usecols = ['name', 'value'])
+    df = pd.read_csv(file,
+            usecols = ['name', 'value'],
+            converters={'value': lambda s: float(s.replace(',', '').replace('$', ''))},
+            thousands=r',')
 
     # print(os.path.splitext(file)[0].split("/")[1])
     df.columns = df.columns.str.strip()
@@ -22,8 +28,37 @@ for file in data_files:
     # dfs.append(df)
 
 print(combined_df)
+
+col_combinations = list(itertools.combinations(cols, 2))
+# pprint.pprint(col_combinations)
+
+results = {}
+for col1, col2 in col_combinations:
+    # print(combined_df[col1].apply(type))
+    try:
+        corr = combined_df[col1].corr(combined_df[col2])
+    except Exception as e:
+        print(e)
+        print(col1, col2)
+        print(combined_df[col1], combined_df[col2])
+        break
+
+    # print(f"correlation between {col1} and {col2}: {corr:.2f}")
+    results[(col1, col2)] = float(f"{corr:.2f}")
+
+# pprint.pprint(results)
+# sorted_dict = dict(sorted(results.items(), key=lambda item: item[1]))
+sorted_dict = [(k, results[k]) for k in sorted(results, key=results.get, reverse=True)]
+
+pprint.pprint(sorted_dict)
+
+
+
 exit(0)
-pprint.pprint(dfs)
-# df = cols[0]
-# for sub_df in cols[1:]:
-    # df = df.merge(sub_df, on='residue')
+
+"""
+ax1 = df.plot.scatter(
+        x='infant_mortality_rate',
+        y='population'
+        )
+"""
